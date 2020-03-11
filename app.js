f322eddabd2f802d0305bbe27bc7223b724153c2
Scott beverly JR <scottbeverlyjr@Scotts-Air.fios-router.home>
2020-03-11 13:43:06 -0400

    Tablet edits

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ let id = '1ABSg_gTOZEj4URDdodS6Mwx5crZhCtaIdlKaHnuZ57A'
 let source = `https://spreadsheets.google.com/feeds/list/1ABSg_gTOZEj4URDdodS6Mwx5crZhCtaIdlKaHnuZ57A/od6/public/values?alt=json`
 
 fetch(source)
-  .then( response => response.json() ) // this parses the data from string back into an object
+  .then( response => response.json() )
   .then( data =>  {
       console.log('data', data)
-      // data.feed.entry is the array that stores our projects
-      // the projects are stored as objects
+    
       let projects = data.feed.entry.map( project => {
-        // console.log('project', project.gsx$title.$t)
+        
         return {
           title: project.gsx$title.$t,
           image: project.gsx$image.$t,
@@ -20,7 +19,7 @@ fetch(source)
         }
       })
       app(projects)
-  }) // this provides us access to the parse data
+  }) 
   .catch( err => console.log('err', err))
 
 function app(projects) {
@@ -36,7 +35,11 @@ function projectData(){
            <a class="project-titles" href="#">${projects[i].title}</a>
         <div class="project-image">
         <a href=${projects[i].url}>
-             <img width="500" height="450" src=${projects[i].image} alt="startup matchmaker"></a>
+             <img width= "100%" height="100%" src=${projects[i].image} alt="startup matchmaker"></a>
+        </div>
+        <div class= buttons>
+        <button href = "#"> Code </button>
+        <button href = "#"> Repo </button>
         </div>
         </article>`)
 
@@ -45,6 +48,8 @@ function projectData(){
   }
   projectData()
  }
+
+
   
 
 
@@ -72,18 +77,3 @@ function projectData(){
 
 
 
-// $(document).ready(function () {
-
-//   $('.first-button').on('click', function () {
-
-//     $('.animated-icon1').toggleClass('open');
-//   });
-//   $('.second-button').on('click', function () {
-
-//     $('.animated-icon2').toggleClass('open');
-//   });
-//   $('.third-button').on('click', function () {
-
-//     $('.animated-icon3').toggleClass('open');
-//   });
-// });
